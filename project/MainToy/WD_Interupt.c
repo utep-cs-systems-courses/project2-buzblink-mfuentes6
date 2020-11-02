b1dc3c8845ba81543c143a3e03fdb04ec0f47667
student <student@systems-vm.localdomain>
2020-11-01 23:02:06 -0700

    more final touches

--- project/MainToy/WD_Interupt.c
+++ project/MainToy/WD_Interupt.c
@@ -1,17 +1,18 @@
 #include <msp430.h>
-#include state_machine.h
-#include buzzer.h
+#include "state_machine.h"
+#include "buzzer.h"
 
 
 
 
 void
-__interupt_vec(WDT_VECTOR)WDT(){
-
+ __interrupt_vec(WDT_VECTOR) WDT(){
+  
   static char blink_count = 0;
   if(++blink_count == speed){
     buzzer_set_period(speed);
     state_advance();
     blink_count = 0;
+  }
 
 }
